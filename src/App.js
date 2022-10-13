416cea81efa3a56073b5f6e9128fd2efd164806a
Volodymyr Ovladieiev <v.ovladieiev@liguriadigitale.it>
2022-10-13 13:51:10 +0200

    Add Full name input and Agree checkbox

--- src/App.js
+++ src/App.js
@@ -3,25 +3,37 @@ import './App.css';
 import FormInput from './components/FormInput';
 
 function App() {
-  const [values, setValues] = React.useState(
-    {
+  const [values, setValues] = React.useState(JSON.parse(localStorage.getItem("regForm")) || 
+  {
+      fullName: "",
       username: "",
       email: "",
       birthday: "",
       password: "",
-      confirmPassword: ""
+      confirmPassword: "",
+      isAgree: false
     }
   )
 
   const inputs =  [
     {
+      id: 0,
+      name: "fullName",
+      type: "text",
+      placeholder: "Name Surname",
+      label: "Full name",
+      errorMessage: "Incorrect full name",
+      pattern: "(^[A-Za-z]{3,16})([ ]{0,1})([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})",
+      required: true
+    },
+    {
       id: 1,
       name: "username",
       type: "text",
       placeholder: "Username",
-      errorMessage: "Username should be 3-16 characters and shouldn't include any special character!",
       label: "Username",
-      pattern: "^[A-za-z0-9]{3,16}$",
+      errorMessage: "Username should be 3-16 characters and shouldn't include any special character!",
+      pattern: "^[A-Za-z0-9]{3,16}$",
       required: true
     },
     {
@@ -29,16 +41,18 @@ function App() {
       name: "email",
       type: "email",
       placeholder: "Email",
-      errorMessage: "It should be a valid email address!",
       label: "Email",
+      errorMessage: "It should be a valid email address!",
       required: true
     },
     {
       id: 3,
       name: "birthday",
       type: "date",
-      placeholder: "Birthday",
-      label: "Birthday"
+      placeholder: "Date of birth",
+      label: "Date of birth",
+      errorMessage: "Select the date of birth",
+      required: true
     },
     {
       id: 4,
@@ -63,11 +77,17 @@ function App() {
   ]
   
   function onChange(e) {
-    return setValues({...values, [e.target.name]: e.target.value})
+    const {name, value, type, checked} = e.target
+    return setValues(prevValues=>({...prevValues, [name]: type === "checkbox" ? checked : value}))
   }
 
   function handleSubmit(e) {
     e.preventDefault()
+    const correctFullName = values.fullName.toLowerCase().split(" ").map(word=>word[0].toUpperCase()+word.slice(1)).join(" ")
+    const correctUsername = values.username.toLowerCase()
+    setValues(prevValues=>({...prevValues, fullName: correctFullName, username: correctUsername}))
+    localStorage.setItem("regForm", JSON.stringify({...values, fullName: correctFullName, username: correctUsername /* password: "", confirmPassword: "" */}))
+    alert(`Full name: ${values.fullName}\nUsername: ${values.username}\nEmail: ${values.email}\nDate of bitrh: ${values.birthday}\n${values.isAgree ? "You are agreed to the terms\n" : ""}Your data is saved`)
   }
 
   return (
@@ -81,6 +101,13 @@ function App() {
             value={values[input.name]}
             onChange={onChange}
           />))}
+          <input
+            type="checkbox"
+            checked={values.isAgree}
+            name="isAgree"
+            onChange={onChange}
+          />
+          <label>I am agree to the <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" title='Definitely not a rickroll link' target="_blank" rel="noreferrer">terms</a></label>
         <button>Submit</button>
       </form>
     </div>
